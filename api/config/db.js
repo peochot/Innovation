e3bfff082221ff132965de95164fd10c3c41ef04
Huy Phan <huy.phan@metropolia.fi>
2016-10-22 00:15:21 +0300

    Fix null in coords

--- api/config/db.js
+++ api/config/db.js
@@ -1,5 +1,5 @@
 const db =require('mongoose');
-var url='mongodb://localhost:27017/cooking';
+var url='mongodb://beochot:Innovation2016@ds063536.mlab.com:63536/innovation';
 db.Promise = require('bluebird');
 if(process.env.PORT){
    url=process.env.MONGOLAB_URI;
