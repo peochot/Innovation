e493fc55abdb339855b4fc83d5192927fd91f8a9
Huy Phan <huy.phan@metropolia.fi>
2016-10-21 17:27:56 +0300

    Fix remote database url

--- api/config/db.js
+++ api/config/db.js
@@ -2,7 +2,7 @@ const db =require('mongoose');
 var url='mongodb://localhost:27017/cooking';
 db.Promise = require('bluebird');
 if(process.env.PORT){
-   url="mongodb://beochot:beochot@ds021166.mlab.com:21166/mahapp";
+   url=process.env.MONGOLAB_URI;
 }
 db.connect(url);
 module.exports = db;
