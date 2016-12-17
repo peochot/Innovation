389361f711db886e98e98eb1f1d75afc69f9b667
Huy Phan <huy.phan@metropolia.fi>
2016-12-17 16:22:00 +0200

    Fix job map component

--- app/components/Map/JobCard.js
+++ app/components/Map/JobCard.js
@@ -2,34 +2,43 @@ import React from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { bookmark,apply,openDiscussModal} from '../../actions';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
 
 const mapDispatchToProps = dispatch => ({
   bookmark: (jobId)=>dispatch(bookmark(jobId)),
   apply: (jobId)=>dispatch(apply(jobId)),
   openDiscussModal:()=>dispatch(openDiscussModal())
 });
-export const JobCard = props => (
-  <div className="container g-t--12">
-    <div style={{width:"70%"}} className="card">
-      <h4>{props.job.title}</h4>
-      {props.job.description}
-    </div>
-    <div style={{width:"30%"}} className="card">
-      {
-          props.job.bookmarked?
-          <div>Bookmarked</div>:
-          <div><button className="btn--float" onClick={()=>props.bookmark(props.job._id)}>+</button></div>
-      }
-      <button className="btn--raised">Share</button>
-      <button className="btn--raised" onClick={()=>props.openDiscussModal()}>Discuss</button>
+export class JobCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {fullDesc: false};
+  }
+  toggleDescription = () =>{ this.setState({fullDesc:!this.state.fullDesc})}
+  render() {
+    return (
+      <Card>
+        <CardHeader
+          title={this.props.job.title}
+          subtitle={this.props.job.company}
+          avatar="images/jsa-128.jpg"
+        />
 
-      <Link className="btn--raised" to={`/job/${props.job._id}/apply`}>Apply</Link>
-      <p>
-        Expire date : {props.job.expire?props.job.expire:"ASAP"}
-      </p>
-    </div>
-  </div>
-);
-
-
+        <CardTitle title="Description" subtitle={"Expire on: "+ this.props.job.expire} />
+        <CardText>
+          {this.state.fullDesc?this.props.job.description:`${this.props.job.description.substring(0, 100)}...`}
+          <RaisedButton label="Toggle" onClick={this.toggleDescription}> </RaisedButton>
+        </CardText>
+        <CardActions>
+        {
+          this.props.job.bookmarked?<div>Bookmarked</div>:
+          <FloatingActionButton onClick={()=>this.props.bookmark(this.this.props.job._id)} label="Ac"></FloatingActionButton>
+        }
+          <FlatButton label="Action2" />
+        </CardActions>
+      </Card>)}
+    }
 export default connect(null,mapDispatchToProps)(JobCard);
